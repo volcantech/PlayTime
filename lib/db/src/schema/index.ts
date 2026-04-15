d82aa688181d718229563511e9e9232552839a9a
Volcan'Tech <lardinoisstevenfl@gmail.com>
2026-04-15 21:54:37 +0200

    Add files via upload

---
+++ lib/db/src/schema/index.ts
@@ -0,0 +1,6 @@
+export * from "./questionCards";
+export * from "./answerCards";
+export * from "./admins";
+export * from "./undercoverWordPairs";
+export * from "./petitBacCategories";
+export * from "./gameSettings";
